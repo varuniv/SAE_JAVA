594ccc905276f123dfe4f95e822cffe3cb630c26
AlexandreGuihard <163568140+AlexandreGuihard@users.noreply.github.com>
2024-06-17 16:54:50 +0200

    BD
    Fichier SQL contenant les tables

--- src/bd/bd.sql
+++ src/bd/bd.sql
@@ -1,18 +1,41 @@
-drop table ATHLETE if exists;
-drop table EPREUVE if exists;
+DROP TABLE IF EXISTS ATHLETE;
+DROP TABLE IF EXISTS PAYS;
+DROP TABLE IF EXISTS EQUIPE;
+DROP TABLE IF EXISTS EPREUVE;
 
-create table ATHLETE(
-    idAthlete int primary key,
-    prenom varchar(20),
-    nom varchar(20),
-    sexe varchar(1),
-    force_ decimal,
-    agilite decimal,
-    endurance decimal,
-    pays varchar(20)
+CREATE TABLE EPREUVE(
+    idEpreuve INT PRIMARY KEY,
+    nomEpreuve VARCHAR(50),
+    categorie VARCHAR(20),
+    enEquipe BOOLEAN,
+    critereForce DECIMAL,
+    critereAgilite DECIMAL,
+    critereEndurance DECIMAL,
+    attributSport VARCHAR(20) -- Si le sport n'a pas d'attributs on initialise à NULL
 );
 
-create table EPREUVE(
-    idEpreuve int primary key,
-    nom varchar(50)
+CREATE TABLE EQUIPE(
+    idEquipe INT PRIMARY KEY,
+    nomEquipe VARCHAR(30),
+    idEpreuve INT,
+    FOREIGN KEY (idEpreuve) REFERENCES EPREUVE(idEpreuve)
+);
+
+CREATE TABLE PAYS(
+    idPays INT PRIMARY KEY,
+    nomPays VARCHAR(15)
+);
+
+CREATE TABLE ATHLETE(
+    idAthlete INT PRIMARY KEY,
+    prenom VARCHAR(20),
+    nom VARCHAR(20),
+    sexe VARCHAR(1),
+    force_ DECIMAL,
+    agilite DECIMAL,
+    endurance DECIMAL,
+    idPays INT,
+    idEquipe INT,
+    FOREIGN KEY (idEquipe) REFERENCES EQUIPE(idEquipe),
+    FOREIGN KEY (idPays) REFERENCES PAYS(idPays)
 );
