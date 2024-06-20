183332f5987f930a25c03e7fe4419d5cee506402
AlexandreGuihard <163568140+AlexandreGuihard@users.noreply.github.com>
2024-06-20 09:01:58 +0200

    Inscription, Connexion

--- src/Vue/ControllerPage.java
+++ src/Vue/ControllerPage.java
@@ -1,13 +1,12 @@
+package main.java.com.cdal.Vue;
+
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonType;
-import java.util.Optional;
 
 public class ControllerPage implements EventHandler<ActionEvent>{
     private Appli appli;
@@ -42,6 +41,22 @@ public class ControllerPage implements EventHandler<ActionEvent>{
     @FXML
     private TextField tfpseudo;
 
+    @FXML
+    private TextField tfnomcompet;
+
+    @FXML
+    private ComboBox<Text> epreuves;
+
+    @FXML
+    private ListView<RadioButton> rd;
+
+    @FXML
+    private RadioButton rdAthlete;
+
+    @FXML
+    private RadioButton rdEquipe;
+
+
 
     @Override
     public void handle(ActionEvent event){
@@ -51,16 +66,53 @@ public class ControllerPage implements EventHandler<ActionEvent>{
                 appli.pageInscription();
             }
             else if (btn.getText().equals("M'inscrire")){
-                appli.pageDeConnexion();
+                try{
+                    String nom = appli.getTNomCo();
+                    String mdp = appli.getTMdpCo();
+                    if (!(tfconfirm.getText().equals("") && tfnom.getText().equals("") && tfpassword.getText().equals("") && tfpays.getText().equals("") && tfprenom.getText().equals("") && tfpseudo.getText().equals(""))){ // Si tous les textfields sont complétés
+                        if (this.tfconfirm.getText().equals(this.tfpassword)){
+                            if (!this.appli.getBD().userInBd(nom, mdp)){
+                                String nom = this.tfnom.getText();
+                                String prenom = this.tfprenom.getText();
+                                String pseudo = this.tfpseudo.getText();
+                                String mdp = this.tfpassword.getText();
+                                this.appli.getBD().insertUtilisateur(nom, prenom, pseudo, mdp);
+                                appli.pageDeConnexion();
+                            }
+                        }
+                    }
+                }
+                catch (Exception e){
+                    System.error.println("Les textfields ne sont pas complétés ")
+                }
+                
             }
             else if (btn.getText().equals("Annuler")) {
                 appli.pageDeConnexion();  
             }
             else if (btn.getText().equals("Se Connecter")){
-                appli.pageConsultation();
+                String nom = appli.getTNomCo();
+                String mdp = appli.getTMdpCo();
+                if (nom.equals("admin") && mdp.equals("admin")){
+                    appli.pageConsultation();
+                }
+                
+                else if(!appli.getBD().userInBd(nom,password)){
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Erreur");
+                    alert.setHeaderText("Erreur de connexion");
+                    alert.setContentText("Nom d'utilisateur ou mot de passe incorrect");
+                    alert.showAndWait();
+                }
+                else{
+                    appli.pageConsultation();
+                }
+
+                appli.setTNomCo();
+                appli.setTMdpCo();
+                
             }
-            else if (btn.getText().equals("Déconnexion")){
-                // Show confirmation popup
+            else if (btn.getText().equals("Déconnexion")){ 
                 Alert alert = new Alert(AlertType.CONFIRMATION);
                 alert.setTitle("Confirmation");
                 alert.setHeaderText("Déconnexion");
@@ -71,10 +123,10 @@ public class ControllerPage implements EventHandler<ActionEvent>{
                     appli.pageDeConnexion();
                 }
             }
-
-            
             else if (btn.getText().equals("Créer compétition")){
+                
                 appli.pageCompetition();
+
             }
             else if (btn.getText().equals("Créer la Compétition")){
                 appli.pageConsultation();
